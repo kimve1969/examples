62f4e7db2c9355354845a2330ae6f28f5439a193
kimve1969 <kimve@mail.ru>
2022-03-17 15:24:50 +0000

    modified fork.cpp

--- fork.cpp
+++ fork.cpp
@@ -1,8 +1,28 @@
 #include<iostream>
+#include<sys/types.h>
+#include<unistd.h>
 
 
 int  main(int argc, char *argv[])
 {
+  pid_t pid = fork();
+  int xcount = 0;
+  if(pid){
+    // TODO for child process
+    std::cout<<"child pid = "<<pid<<std::endl;
+    while(true){
+      sleep(2);
+      std::cout<<xcount++<<std::endl;
+    }
+  }
+  else{
+    // TODO for current process
+    std::cout<<"parent pid = "<<pid<<std::endl;
+    while(true){
+      sleep(2);
+      std::cout<<xcount++<<std::endl;
+    }
+  }
   
   return 0;
 }
