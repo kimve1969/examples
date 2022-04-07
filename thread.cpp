7c560801def644930eb2b1a61b9ab504fe651e02
kimve1969 <kimve@mail.ru>
2022-04-07 17:46:16 +0000

    add thread.cpp

---
+++ thread.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<thread>
+#include<atomic>
+
+// clang thread.cpp -o thread -lstdc++ -ptherad -fsanitize=thread -fPIC
+//int gint = 0; // concurrent is bad
+std::atomic<int> gint(0); // concurrent is ok 
+
+void incr(){
+  for(int i=0; i<1'000'000; ++i) ++gint;
+} 
+
+int main(int argc, char *argv[])
+{
+  std::thread th[2]{std::thread(incr), std::thread(incr)};
+
+  
+  th[0].join();
+  th[1].join();
+
+  std::cout<<"gint = "<<gint<<"\n";
+  
+  return 0;
+}
+
